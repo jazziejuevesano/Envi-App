507e2dbe6e60a40470abf6c1bf465c2222146fbe
Jazzie Juevesano <jazziejuevesano13@gmail.com>
2020-07-29 11:37:51 -0400

    initial commit

---
+++ Envi App/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  Envi App
+//
+//  Created by Jazzie Juevesano on 7/29/20.
+//  Copyright © 2020 Jazzie Juevesano. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
